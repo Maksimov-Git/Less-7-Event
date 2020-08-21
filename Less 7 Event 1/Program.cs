fc9310e58512192a5717e69daf261ad5a7d1b6cd
Maksimov-Git <maksimov_alexey@inbox.ru>
2020-08-21 16:53:45 +0300

    добавлено два примера на события

---
+++ Less 7 Event 1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less_7_Event_1
+{
+    public delegate void EventDelegate();
+
+    public class MyClass
+    {
+        EventDelegate myEvent = null;
+        // ключевое слово EVENT, указывает на то, что поле(уже не поле) класса  является событием
+        public event EventDelegate MyEvent
+        {
+            add 
+            {
+            myEvent += value;
+            }
+            remove
+            {
+            myEvent -= value; 
+            }
+        }
+
+        public void InvokeEvent()
+        {
+            myEvent.Invoke();
+        }
+
+    }
+    class Program
+    {
+        static private void Handler1() // handler - обработчик
+        {
+            Console.WriteLine("произошло событие 1");
+        }
+        // методы обработчики событий
+        static private void Handler2()
+        {
+            Console.WriteLine("произошло событие 2");
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            myClass.MyEvent += new EventDelegate(Handler1);
+            
+            myClass.MyEvent += Handler2;
+
+            myClass.InvokeEvent();
+
+
+        }
+    }
+}
---
+++ Less 7 Event/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less_7_Event
+{
+    public delegate void EventDelegate();
+
+    public class MyClass
+    {
+    // ключевое слово EVENT, указывает на то, что поле(уже не поле) класса  является событием
+     public event EventDelegate myEvent = null;
+
+        public void InvokeEvent()
+        {
+            myEvent.Invoke();
+        }
+
+    }
+
+    class Program
+    {
+        static private void Handler1() // handler - обработчик
+        {
+            Console.WriteLine("произошло событие 1");
+        }
+        // методы обработчики событий
+        static private void Handler2()
+        {
+            Console.WriteLine("произошло событие 2");
+        }
+
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            myClass.myEvent += new EventDelegate(Handler1);
+            // создаем экземпляр класса EventDelegate и сообщаем с ним обработчик  Handler1 и подписываем 
+            // обработчик на событие  myEvent
+           
+            // предположение делегата
+            myClass.myEvent += Handler2;
+
+            myClass.InvokeEvent();
+
+            Console.WriteLine(" ");
+
+            myClass.myEvent -= Handler1;
+            myClass.InvokeEvent();
+
+            Console.ReadKey();
+        }
+    }
+}
